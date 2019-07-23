d28316ca309e11926305c1a767910285507eb030
Muhammad Jaffar <smjr786@gmail.com>
2019-07-23 12:34:45 +0500

    Update SortAndSearch.java
    Buble sort

--- SortAndSearch.java
+++ SortAndSearch.java
@@ -12,4 +12,19 @@ public static void binSea(int[] sortedArray,int x,int l,int r){
             System.out.print(" Not Found ");
         }
     }
+        
+        // Traverse all the array till start of sorted array at the right and swap elements in between if necessory
+        public static void bubSor(int[] array){
+        for(int i=0;i<array.length;i++){
+            for(int j=0;j<array.length-i-1;j++){
+                if (array[j]>array[j+1]){
+                    int temp = array[j+1];
+                    array[j+1]=array[j];
+                    array[j]=temp;
+                }
+            }
+        }
+        System.out.print(Arrays.toString(array));
+    }
+        
     }
